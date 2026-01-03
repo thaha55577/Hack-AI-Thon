8b2476284f0463ebbbdce37ca7c42586ba19144a
thaha55577 <shaikthaha2005@gmail.com>
2026-01-03 21:45:17 +0530

    vite updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HACK-AI-THON/',
+  base: '/Hack-AI-Thon/',
   assetsInclude: ['**/*.MP4'],
   optimizeDeps: {
     exclude: ['lucide-react'],
